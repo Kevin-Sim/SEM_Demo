34b5bc900a36427639444a7cde5d3f383dbd763a
kevinsim <k.sim@napier.ac.uk>
2021-01-28 12:28:01 +0000

    Changed App Code

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,6 @@ package com.napier.sem;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello World again");
+        System.out.println("SET08803 Hello");
     }
 }
