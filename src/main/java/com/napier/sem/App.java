739116a597aab1fd8fd0a8f248a954c5dc0c5784
kevinsim <k.sim@napier.ac.uk>
2021-02-16 08:33:50 +0000

    added get salaries by role

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -22,7 +22,10 @@ public class App {
 //        a.printSalaryReport();
 //        a.printSalaryReportByDept("d005");
 
-        ArrayList<Employee> employees = a.getAllSalaries();
+//        ArrayList<Employee> employees = a.getAllSalaries();
+//        a.printSalaries(employees);
+
+        ArrayList<Employee> employees = a.getAllSalaries("Manager");
         a.printSalaries(employees);
 
         // Disconnect from database
@@ -277,7 +280,7 @@ public class App {
                     "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
                             + "FROM employees, salaries "
                             + "WHERE employees.emp_no = salaries.emp_no AND salaries.to_date = '9999-01-01' "
-                            + "ORDER BY employees.emp_no ASC";
+                            + "ORDER BY employees.emp_no ASC;";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract employee information
@@ -300,6 +303,48 @@ public class App {
             return null;
         }
     }
+
+    /**
+     * Gets all the current employees and salaries in a particular role (titles.title).
+     * @return A list of all employees and salaries, or null if there is an error.
+     */
+    public ArrayList<Employee> getAllSalaries(String role)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    " SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary, titles.title" +
+                            " FROM employees, salaries, titles WHERE employees.emp_no = salaries.emp_no " +
+                            "AND salaries.to_date = '9999-01-01' AND titles.to_date = '9999-01-01'" +
+                            "AND titles.emp_no = employees.emp_no AND titles.title = '" + role +
+                            "' ORDER BY employees.emp_no ASC;";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                emp.title = rset.getString("titles.title");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
     /**
      * Prints a list of employees.
      * @param employees The list of employees to print.
@@ -307,13 +352,13 @@ public class App {
     public void printSalaries(ArrayList<Employee> employees)
     {
         // Print header
-        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        System.out.println(String.format("%-10s %-15s %-20s %-8s %-15s" , "Emp No", "First Name", "Last Name", "Salary", "Title"));
         // Loop over all employees in the list
         for (Employee emp : employees)
         {
             String emp_string =
-                    String.format("%-10s %-15s %-20s %-8s",
-                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+                    String.format("%-10s %-15s %-20s %-8s %-15s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary, emp.title);
             System.out.println(emp_string);
         }
     }
