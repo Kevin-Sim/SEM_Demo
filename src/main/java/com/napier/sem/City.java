e6ac6ffdb6ea0ddc1a19eaa2751ee9a152e35c20
kevinsim <k.sim@napier.ac.uk>
2021-02-24 08:49:55 +0000

    Started creating world classes

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class City {
+    int ID;
+    String Name;
+    String CountryCode;
+    String District;
+    int Population;
+
+}
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,8 @@
+package com.napier.sem;
+
+public class Country {
+    String Code;
+    String Name;
+    ,Continent,Region,SurfaceArea,IndepYear,Population,LifeExpectancy,GNP,GNPOld,LocalName,GovernmentForm,HeadOfState,Capital,Code2
+
+}
